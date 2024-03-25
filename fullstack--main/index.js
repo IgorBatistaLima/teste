28c790788cd784307bae281f7cb9793977567251
Igor Batista Lima <112262600+IgorBatistaLima@users.noreply.github.com>
2024-03-25 11:48:13 -0300

    Update index.js

--- fullstack--main/index.js
+++ fullstack--main/index.js
@@ -2,17 +2,18 @@
 const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 const targetUrl = `https://www.cheapshark.com/api/1.0/deals?storeID=&upperPrice=15`;
 
+
 fetch(proxyUrl + targetUrl)
   .then((response) => response.json())
   .then((data) => {
-    
+
   })
   .catch((error) => console.log("error", error));
 
 function getDeals() {
   const storeSelect = document.getElementById("store");
   const selectedStoreId = storeSelect.value;
- 
+
 
   var requestOptions = {
     method: "GET",
@@ -23,7 +24,7 @@ function getDeals() {
     `https://www.cheapshark.com/api/1.0/deals?storeID=${selectedStoreId}&upperPrice=15`,
     requestOptions
   )
-    .then((response) => response.json()) 
+    .then((response) => response.json())
     .then((data) => {
       const dealsContainer = document.getElementById("deals");
       dealsContainer.innerHTML = "";
@@ -66,53 +67,56 @@ fetch("https://www.cheapshark.com/api/1.0/stores")
 
   .catch((error) => console.log("error", error));
 
-  function searchGame(event) {
-    event.preventDefault();
-  
-    let searchInput = document.getElementById("search-input").value;
-    searchInput = encodeURIComponent(searchInput);
-  
-    fetch(`https://www.cheapshark.com/api/1.0/games?title=${searchInput}`)
-      .then((response) => response.json())
-      .then((data) => {
-        if (Array.isArray(data)) {
-          data.forEach(game => {
-            const dealId = game.cheapestDealID;
-            dealLookup(dealId);
-          });
-        } else {
-          console.log("Data is not an array");
-        }
-      });
-  }
-  
-  function dealLookup(dealId) {
-    fetch(`https://www.cheapshark.com/api/1.0/deals?id=${dealId}`)
-      .then((response) => response.json())
-      .then((data) => {
-        if (data && data.gameInfo) {
-          const gameInfo = data.gameInfo;
-          const dealsContainer = document.getElementById("deals");
-  
-          const dealElement = document.createElement("div");
-          dealElement.classList.add("deal");
-  
-          dealElement.innerHTML = `
+function searchGame(event) {
+  event.preventDefault();
+
+  let searchInput = document.getElementById("search-input").value;
+  searchInput = encodeURIComponent(searchInput);
+
+  fetch(`https://www.cheapshark.com/api/1.0/games?title=${searchInput}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data && data.length > 0) {
+        data.forEach(game => {
+          const dealId = game.cheapestDealID;
+          dealLookup(dealId);
+        });
+      } else {
+        console.log("No games found with that name");
+      }
+    });
+}
+
+function dealLookup(dealId) {
+
+  fetch(`https://www.cheapshark.com/api/1.0/deals?id=${dealId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data && data.gameInfo) {
+        const gameInfo = data.gameInfo;
+        const dealsContainer = document.getElementById("deals");
+
+
+        const dealElement = document.createElement("div");
+        dealElement.classList.add("deal");
+
+        dealElement.innerHTML = `
           <h2>${gameInfo.name}</h2>
           <img src="${gameInfo.thumb}">
           <p>Normal Price: $${gameInfo.retailPrice}</p>
+          <p>Sale Price: $${gameInfo.salePrice}</p>
           <p>Steam Rating: ${gameInfo.steamRatingText}</p>
           <p>Steam Reviews: ${gameInfo.steamRatingCount}</p>
           <p>DealID: ${dealId}</p>
-          
+          <a href="https://www.cheapshark.com/redirect?dealID=${dealId}" target="_blank">View Deal</a>
           `;
-          dealsContainer.appendChild(dealElement);
-        } else {
-          console.log("No deal found with that ID");
-        }
-      })
-      .catch((error) => console.log("error", error));
-  }
+        dealsContainer.appendChild(dealElement);
+      } else {
+        console.log("No deal found with that ID");
+      }
+    })
+    .catch((error) => console.log("error", error));
+}
 
-  const searchForm = document.getElementById("search-form");
-  searchForm.addEventListener("submit", searchGame);
+const searchForm = document.getElementById("search-form");
+searchForm.addEventListener("submit", searchGame);
